9c2301488293481f9531c17924450950573fe72c
Arpit Mishra <146555776+Soap-mac@users.noreply.github.com>
2024-12-09 00:53:21 +0530

    all projects updated

--- all_projects.js
+++ all_projects.js
@@ -1,13 +1,28 @@
 const options = document.querySelectorAll('.mainarea .leftside div');
 const wishing = document.querySelector('.wish');
-// const option1 = document.querySelector('.option1');
-// const option2 = document.querySelector('.option2');
-// const option3 = document.querySelector('.option3');
 const dropdown = document.querySelector('.dropdown');
-const dropdownmenus = document.querySelectorAll('.dropdown-hover');
+const option1 = document.querySelector('.option1');
+const option2 = document.querySelector('.option2');
+const option3 = document.querySelector('.option3');
 const navmenu = document.querySelector('.nav-menu');
-const dropdownShow = document.querySelector('.dropdown-show');
-const projectDropdown = document.querySelector('.project-dropdown');
+const newbtn = document.querySelector('.newproject');
+const popup = document.querySelector('.popup-container');
+const closepopup = document.querySelector('.close-btn i');
+const createbtn = document.querySelector('.btn');
+const projectName = document.querySelector('.project-name');
+const projectDesc = document.querySelector('.project-desc');
+const nameWarning = document.querySelector('.name-warning');
+const descWarning = document.querySelector('.desc-warning');
+const gridarea = document.querySelector('.gridarea');
+const gridpic = document.querySelector('.gridpic');
+const boxicon = document.querySelectorAll('.gridicon');
+
+
+const elements = [];
+const max = 3;
+const usedNames = new Set();
+
+
 
 options.forEach(option => {
 
@@ -19,21 +34,22 @@ options.forEach(option => {
 
 });
 
-// option1.addEventListener('click',()=>{
-//     option1.classList.add('selected');
-//     option2.classList.remove('selected');
-//     option3.classList.remove('selected');
-// });
-// option2.addEventListener('click',()=>{
-//     option1.classList.remove('selected');
-//     option2.classList.add('selected');
-//     option3.classList.remove('selected');
-// });
-// option3.addEventListener('click',()=>{
-//     option1.classList.remove('selected');
-//     option2.classList.remove('selected');
-//     option3.classList.add('selected');
-// });
+option1.addEventListener('click',()=>{
+    option1.classList.add('selected');
+    option2.classList.remove('selected');
+    option3.classList.remove('selected');
+});
+option2.addEventListener('click',()=>{
+    option1.classList.remove('selected');
+    option2.classList.add('selected');
+    option3.classList.remove('selected');
+});
+option3.addEventListener('click',()=>{
+    option1.classList.remove('selected');
+    option2.classList.remove('selected');
+    option3.classList.add('selected');
+});
+
 
 navmenu.addEventListener('mouseenter',()=>{
     dropdown.style.display='block';
@@ -47,6 +63,78 @@ navmenu.addEventListener('mouseleave',()=>{
 })
 dropdown.addEventListener('mouseleave',()=>{
     dropdown.style.display='none';
+})
+
+newbtn.addEventListener('click',()=>{
+    projectName.value = "";
+    projectDesc.value = "";
+    popup.style.display='block';
+})
+
+closepopup.addEventListener('click',()=>{
+    popup.style.display='none';
+})
+
+function toggleHeartColor(event) {
+    const heart = event.target;
+
+    const currentColor = getComputedStyle(heart).color;
+
+    if (currentColor === 'rgb(255, 0, 0)') {
+        heart.style.color = 'black';
+    } else {
+        heart.style.color = 'red';
+    }
+}
+
+boxicon.forEach(heart => {
+    heart.addEventListener('click', toggleHeartColor);
+});
+
+createbtn.addEventListener('click',()=>{
+    console.log(projectName.value);
+    nameWarning.innerHTML="";
+    if(!projectName.value){
+        nameWarning.innerHTML="project name is required";
+        descWarning.innerHTML="";
+        return;
+    }
+    
+    let gridpic = document.createElement('div');
+    gridpic.classList.add('gridpic');
+
+    let gridicon = document.createElement('i');
+    gridicon.classList.add('fa-solid','fa-heart','gridicon');
+    gridicon.addEventListener('click', toggleHeartColor);
+    gridpic.appendChild(gridicon);
+
+    let gridtitle = document.createElement('p');
+    if(usedNames.has(projectName.value)){
+        nameWarning.innerHTML="This project already exists";
+        return;
+    }
+    usedNames.add(projectName.value);
+    gridtitle.innerText = projectName.value;
+
+    let gridname = document.createElement('div');
+    gridname.classList.add('gridname');
+    gridname.appendChild(gridtitle);
+
+    let newgrid = document.createElement('div');
+    newgrid.classList.add('boxes')
+    gridarea.insertBefore(newgrid,gridarea.firstChild);
+    popup.style.display='none';
+
+    newgrid.appendChild(gridpic);
+    newgrid.appendChild(gridname);
+
+    elements.push(newgrid);
+    if(elements.length>max){
+        const oldest = elements.shift();
+        gridarea.removeChild(oldest);
+    }
+
+
 })
 
 function greeting() {
