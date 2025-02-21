108728efb3722de36df6261086c4627d4fab1a96
XBastille <148121344+XBastille@users.noreply.github.com>
2025-02-21 14:07:48 +0530

    inpaint-model done

---
+++ deepfx/inpainting/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inpaint_view, name='inpaint'),
+    path('api/generate-mask/', views.generate_mask, name='generate_mask'),
+    path('api/save-mask/', views.save_mask, name='save_mask'),
+    path('api/process-image/', views.process_image, name='process_image'),
+]
