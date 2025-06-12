2c93ac767a72210c9c43d101a48977d949092bc5
XBastille <148121344+XBastille@users.noreply.github.com>
2025-06-12 13:42:03 +0530

    Delete nst_project directory

--- nst_project/nst_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'nst_app'
-
-urlpatterns = [
-    path('', views.index, name='nst'),
-    path('process/', views.process_images, name='process_images'),
-]
