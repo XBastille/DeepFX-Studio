46e96aa1d5ebf9a902041414ea0c51fcc7fad19c
XBastille <148121344+XBastille@users.noreply.github.com>
2025-08-21 21:09:32 +0530

    Delete ai_image_upscale directory

--- ai_image_upscale/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiImageUpscaleConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ai_image_upscale"
--- ai_image_upscale/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "ai-image-upscale"
-
-urlpatterns = [
-    path("", views.ai_image_upscale, name="ai_image_upscale"),
-]
