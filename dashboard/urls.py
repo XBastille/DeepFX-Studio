d8cabee3b369c86baea799bdbc53a85df1365bcf
XBastille <148121344+XBastille@users.noreply.github.com>
2025-08-21 20:38:31 +0530

    Delete dashboard directory

--- dashboard/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "dashboard"
-
-urlpatterns = [path("dashboard/", views.dashboard_view, name="dashboard_view")]
--- dashboard/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-# Create your views here.
-
-
-@login_required
-def dashboard_view(request):
-    return render(request, "pages/dashboard.html")
