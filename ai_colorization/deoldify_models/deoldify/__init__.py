e0e3fa3efab5b38d6cbb0f82f652153fecd2d570
Bibhor Puhan <eziopuhan825@gmail.com>
2025-08-22 13:22:03 +0000

    wipeout

--- ai_colorization/deoldify_models/deoldify/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-import sys
-
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-logging.getLogger().setLevel(logging.INFO)
-
-from ai_colorization.deoldify_models.deoldify._device import ComputeManager
-
-device = ComputeManager()
